fc95efff709cd071de0596c08a89a1ea5761ed28
Unknown <xoppyksol3@gmail.com>
2018-01-25 23:24:04 +0000

    Verificacao Navbar

--- resources/assets/js/components/navbar.vue
+++ resources/assets/js/components/navbar.vue
@@ -2,14 +2,23 @@
   <nav class="navbar" role="navigation" aria-label="main navigation">
       <div class="navbar-brand">
           <router-link class="navbar-item" to="/blackJack">Black Jack</router-link>
-          <router-link class="navbar-item" to="/login">Login</router-link>
-          <router-link class="navbar-item" to="/regist">Regist</router-link>
+          <router-link class="navbar-item" v-if="!isAuth" to="/login">Login</router-link>
+          <router-link class="navbar-item" v-if="!isAuth" to="/regist">Regist</router-link>
+          <a class="navbar-item" v-if="isAuth" v-on:click.prevent="logout()">Logout</a>
       </div>
   </nav>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      isAuth: false
+    }
+  },
+  created () {
+    this.isAuth = this.$auth.isAuthentificated()
+  }
 }
 </script>
 
